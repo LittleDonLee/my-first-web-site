7619bfb4582fc9c53003eca480801156b53f6646
LittleDonLee <115536863+LittleDonLee@users.noreply.github.com>
2023-01-11 19:15:50 +0900

    웹 페이지 완성

---
+++ js/weather.js
@@ -0,0 +1,15 @@
+/* const API_KEY="da5a099c8fb319bf41e24ff7188305ff";
+               da5a099c8fb319bf41e24ff7188305ff
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("you live in",lat,lng);  
+
+}
+function onGeoError(){
+alert("Can't find you. NO WEATHER FOR YOU")
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError); 
+*/
